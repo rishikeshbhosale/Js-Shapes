c13dd1304d5b7a365c222ad81dccc5484c709ef4
rishikeshbhosale <rishikeshbhosale@gmail.com>
2022-07-01 20:01:03 +0530

    Add files via upload

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,10 @@
 // import using require
+var Shape = require("./shape");
 
 // declare class
-
+class Circle extends Shape{
+    constructor(){super();this.calculateArea=()=>{}}
+ };
 
 // export class using module.exports
+module.exports=Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,10 @@
 // declare class
-
-// export class using module.exports
+class Shape{
+    constructor(){
+      this.color="black";
+    }
+    drawShape(){}
+    calculateArea(){}
+  };
+  module.exports=Shape;
+  // export class using module.exports
